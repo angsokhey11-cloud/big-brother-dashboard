6d0eb6c874836fcaf9114146beb96a4c46a4f71b
Big Brother <angsokhey11@gmail.com>
2026-09-21 15:16:59 +0700

    Bump mobile calculator route for no-scroll price focus

--- mobile-main-menu-v3.js
+++ mobile-main-menu-v3.js
@@ -136,7 +136,7 @@ const MOBILE={
 };
 
 const PC={
- 'sales-support-calculator':'https://angsokhey11-cloud.github.io/big-brother-sales-support/?embed=1&view=calculator&v=20260921-calcfix3',
+ 'sales-support-calculator':'https://angsokhey11-cloud.github.io/big-brother-sales-support/?embed=1&view=calculator&v=20260921-calcfix4',
  'sales-support-request-delivery':'https://angsokhey11-cloud.github.io/big-brother-sales-support/?embed=1&view=request-delivery&v=110',
  'sales-support-your-customer':'https://angsokhey11-cloud.github.io/big-brother-sales-support/?embed=1&view=your-customer&v=110',
  'sales-support-successful-delivery':'https://angsokhey11-cloud.github.io/big-brother-sales-support/?embed=1&view=successful-delivery&v=110',
