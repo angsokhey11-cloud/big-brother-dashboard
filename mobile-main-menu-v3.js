bc59fc4da42bdfcb393c7ceec46c368f3aed9b98
Big Brother <angsokhey11@gmail.com>
2026-09-24 22:12:17 +0700

    Refresh duplicate-safe Invoice Reversal route

--- mobile-main-menu-v3.js
+++ mobile-main-menu-v3.js
@@ -84,7 +84,7 @@ const META={
 const MOBILE={
  'invoice':'https://angsokhey11-cloud.github.io/big-brother-invoice-generator/mobile.html?embed=1&v=20260924-hide-source-no-zero1',
  'history':'https://angsokhey11-cloud.github.io/invoice-history/invoice-history-mobile.html?embed=1&v=20260924-linkededit1',
- 'sale-return':'https://angsokhey11-cloud.github.io/big-brother-invoice-reversal/mobile.html?embed=1&v=20260915-1',
+ 'sale-return':'https://angsokhey11-cloud.github.io/big-brother-invoice-reversal/mobile.html?embed=1&v=20260924-invoiceid1',
  'reversal-history':'https://angsokhey11-cloud.github.io/big-brother-invoice-reversal/mobile-history.html?embed=1&v=20260915-1',
  'payment-history':'https://angsokhey11-cloud.github.io/big-brother-payment-history/mobile.html?embed=1&v=20260915-1',
  'daily-sale-summary':'https://angsokhey11-cloud.github.io/big-brother-daily-sale-summary/mobile.html?embed=1&v=20260915-1',
